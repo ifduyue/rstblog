c31277fe22b333756500c8a204eeb119db1fe635
duyue <ifduyue@gmail.com>
2013-03-07 01:42:13 +0800

    code-block \w linenos, linenostep & hl_lines(emphasize-lines)

--- rstblog/modules/pygments.py
+++ rstblog/modules/pygments.py
@@ -16,10 +16,10 @@ from docutils import nodes
 from docutils.parsers.rst import Directive, directives
 
 from pygments import highlight
-from pygments.lexers import get_lexer_by_name, TextLexer
+from pygments.lexers import get_lexer_by_name, guess_lexer
+from pygments.lexers import PythonLexer, PythonConsoleLexer, CLexer, TextLexer, RstLexer
 from pygments.formatters import HtmlFormatter
 from pygments.styles import get_style_by_name
-
 
 html_formatter = None
 
@@ -29,16 +29,75 @@ class CodeBlock(Directive):
     required_arguments = 1
     optional_arguments = 0
     final_argument_whitespace = False
+    option_spec = {
+        'linenos': directives.flag,
+        'linenostep': directives.positive_int,
+        'emphasize-lines': directives.unchanged_required,
+    }
+    lexers = dict(
+        none = TextLexer(),
+        python = PythonLexer(),
+        pycon = PythonConsoleLexer(),
+        pycon3 = PythonConsoleLexer(python3=True),
+        rest = RstLexer(),
+        c = CLexer(),
+    )
 
     def run(self):
+        code = u'\n'.join(self.content)
+        lang = self.arguments[0].strip()
+        linenos = 'linenos' in self.options
+        linespec = self.options.get('emphasize-lines') or self.options.get('hl_lines')
+        linenostep = self.options.get('linenostep')
+        if linespec:
+            try:
+                nlines = len(self.content)
+                hl_lines = [x+1 for x in self.parselinenos(linespec, nlines)]
+            except ValueError, err:
+                document = self.state.document
+                return [document.reporter.warning(str(err), line=self.lineno)]
+        else:
+            hl_lines = None
+        kwargs = {
+            'hl_lines': hl_lines,
+            'linenos': linenos,
+            'language': lang,
+        }
+        if linenostep:
+            kwargs['linenostep'] = linenostep
         try:
-            lexer = get_lexer_by_name(self.arguments[0])
+            if lang in self.lexers:
+                lexer = self.lexers[lang]
+            elif lang == 'guess':
+                lexer = guess_lexer(code)
+            else:
+                lexer = get_lexer_by_name(lang)
         except ValueError:
             lexer = TextLexer()
-        code = u'\n'.join(self.content)
-        formatted = highlight(code, lexer, html_formatter)
+        formatter = html_formatter(**kwargs)
+        formatted = highlight(code, lexer, formatter)
         return [nodes.raw('', formatted, format='html')]
 
+    def parselinenos(self, spec, total):
+        """Parse a line number spec (such as "1,2,4-6") and return a list of
+        wanted line numbers.
+        """
+        items = list()
+        parts = spec.split(',')
+        for part in parts:
+            try:
+                begend = part.strip().split('-')
+                if len(begend) > 2:
+                    raise ValueError
+                if len(begend) == 1:
+                    items.append(int(begend[0])-1)
+                else:
+                    start = (begend[0] == '') and 0 or int(begend[0])-1
+                    end = (begend[1] == '') and total or int(begend[1])
+                    items.extend(xrange(start, end))
+            except Exception:
+                raise ValueError('invalid line number spec: %r' % spec)
+        return items
 
 def inject_stylesheet(context, **kwargs):
     context.add_stylesheet('_pygments.css')
@@ -52,7 +111,15 @@ def write_stylesheet(builder, **kwargs):
 def setup(builder):
     global html_formatter
     style = get_style_by_name(builder.config.root_get('modules.pygments.style'))
-    html_formatter = HtmlFormatter(style=style)
+    class formatter_partial:
+        def __init__(self, **kwargs):
+            self.kwargs = kwargs
+            self.get_style_defs = HtmlFormatter(**self.kwargs).get_style_defs
+        def __call__(self, **kwargs):
+            kw = self.kwargs.copy()
+            kw.update(kwargs)
+            return HtmlFormatter(**kw)
+    html_formatter = formatter_partial(style=style)
     directives.register_directive('code-block', CodeBlock)
     directives.register_directive('sourcecode', CodeBlock)
     before_file_processed.connect(inject_stylesheet)
